9f77c149e623ec22ad626488274a0a7d9d3e10cf
Vishal Soni <vs332837@gmail.com>
2023-05-05 01:26:20 +0530

    updated colun

--- src/screens/Homescreen.js
+++ src/screens/Homescreen.js
@@ -60,7 +60,7 @@ const Homescreen = () => {
             try {
                 setloading(true);
                 // const data = (await axios.get('/api/rooms/getallrooms')).data
-                const data = (await axios.get('https://thehotelbackend.onrender.com/api/rooms/getallrooms')).data
+                const data = (await axios.get('/api/rooms/getallrooms')).data
                 // console.log(data)
                 //firstly we will get a object and inside there my data is inside data so use like above 
                 setrooms(data)
