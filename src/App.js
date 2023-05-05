1871bccb19eef8c143e20e295457a4084020dc4d
Vishal Soni <vs332837@gmail.com>
2023-05-05 08:47:53 +0530

    updated url

--- src/App.js
+++ src/App.js
@@ -21,7 +21,9 @@ function App() {
           {/* <Route path='/book/:roomid' exact Component={Bookingscreen} /> */}
           <Route path='/book/:roomid/:fromdate/:todate' exact Component={Bookingscreen} />
           <Route path='/register' exact Component={Registerscreen} />
-          <Route path='/login' Component={Loginscreen} />
+          {/* <Route path='/login' exact Component={Loginscreen} /> */}
+          <Route path='/login' element={<Loginscreen />} />
+
           <Route path='/profile' exact Component={Profilescreen} />
           <Route path='/admin' exact Component={Adminscreen} />
         </Routes>
