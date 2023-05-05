eb072349bcef02e0f3aa4850663188f994181ba3
Vishal Soni <vs332837@gmail.com>
2023-05-05 09:29:10 +0530

    updated url

--- src/App.js
+++ src/App.js
@@ -16,15 +16,25 @@ function App() {
       <BrowserRouter>
         <Routes>
 
-          <Route path='/' exact Component={Landingscreen} />
-          <Route path='/home' exact Component={Homescreen} />
+          {/* <Route path='/' exact Component={Landingscreen} /> */}
+          {/* <Route path='/home' exact Component={Homescreen} /> */}
           {/* <Route path='/book/:roomid' exact Component={Bookingscreen} /> */}
-          <Route path='/book/:roomid/:fromdate/:todate' exact Component={Bookingscreen} />
+          {/* <Route path='/book/:roomid/:fromdate/:todate' exact Component={Bookingscreen} />
           <Route path='/register' exact Component={Registerscreen} />
           <Route path='/login' exact Component={Loginscreen} />
 
           <Route path='/profile' exact Component={Profilescreen} />
-          <Route path='/admin' exact Component={Adminscreen} />
+          <Route path='/admin' exact Component={Adminscreen} /> */}
+
+          <Route path='/' element={Landingscreen} />
+          <Route path='/home' element={Homescreen} />
+          {/* <Route path='/book/:roomid' exact Component={Bookingscreen} /> */}
+          <Route path='/book/:roomid/:fromdate/:todate' exact Component={Bookingscreen} />
+          <Route path='/register' element={Registerscreen} />
+          <Route path='/login' element={Loginscreen} />
+
+          <Route path='/profile' element={Profilescreen} />
+          <Route path='/admin' element={Adminscreen} />
         </Routes>
       </BrowserRouter>
     </div>
